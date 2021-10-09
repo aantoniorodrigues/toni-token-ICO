c990aa2a2a549ce9fdd5f3e58add21a616630a80
aantoniorodrigues <antoniocvpr@gmail.com>
2021-10-09 15:21:43 +0100

    built the approve function. all tests passed. started building the trasnferFrom function.

--- contracts/ToniToken.sol
+++ contracts/ToniToken.sol
@@ -11,11 +11,19 @@ contract ToniToken {
 
     // Mapping to store account's balances (substitutes the mandatory "balanceOf" function).
     mapping(address => uint256) public balanceOf;
+    // Mapping to store account's allowance (substitutes the mandatory "allowance" function).
+    mapping(address => mapping(address => uint256)) public allowance;   // mapping structure: owner => { spender: value}
 
     // Event to be triggered when a transfer is made.
     event Transfer(
         address indexed _from,
         address indexed _to,
+        uint256 _value
+    );
+    // Event to be triggered when a delegated transfer is approved.
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
         uint256 _value
     );
 
@@ -27,16 +35,34 @@ contract ToniToken {
         totalSupply = _totalSupply;
     }
 
-    // Function to transfer tokens from one account to another.
+    // Transfer tokens from one account to another.
     function transfer(address _to, uint256 _value) public returns (bool success) {
-        // Throws an exception if the sender doesn't have enough tokens.
+        // Throws an exception if the sender doesn't own enough tokens.
         require(balanceOf[msg.sender] >= _value);
         //Deduct the value from sender and add it to receiver.
         balanceOf[_to] += _value;
         balanceOf[msg.sender] -= _value;
-        // Trigger a transfer event.
+        // Trigger a "Transfer" event.
         emit Transfer(msg.sender, _to, _value);
-        // Return the boolean value (mandatory in ERC20 protocol).
+        // Return a boolean value (mandatory in ERC20 token protocol).
+        return true;
+    }
+
+    // Approves a delegated transfer.
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+        // Set the spender's allowance.
+        allowance[msg.sender][_spender] = _value;
+        // Trigger an "Approve" event.
+        emit Approval(msg.sender, _spender, _value);
+        // Return a boolean value (mandatory in ERC20 token protocol).
+        return true;
+    }
+
+    // Transfer tokens on behalf of another account.
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        // Trigger a "Transfer" event.
+        emit Transfer(_from, _to, _value);
+        // Return a boolean value (mandatory in ERC20 token protocol).
         return true;
     }
 
--- test/ToniToken.js
+++ test/ToniToken.js
@@ -43,26 +43,66 @@ contract('ToniToken', (accounts) => {
 
     // Test if an account's able to transfer more tokens than the ones it owns.
     it('not allowed to transfer more tokens than the ones it owns', () => {
-        return this.ToniToken.transfer.call(accounts[1], 999999999).then(assert.fail).catch((error) => {
+        // Transfer an ammount bigger than the total supply of tokens.
+        return this.ToniToken.transfer.call(accounts[1], 999999999).then(assert.fail).catch((error) => {    // the "call" method doesn't create a transaction, just calls the method
+            // Check if it throws a 'revert' error/exception.
             assert(error.message.indexOf('revert') >= 0, 'error message must contain revert');
         });
     })
 
-    // Test the transfer function.
-    it('transfer function', async () => {
+    // Test the "transfer" function.
+    it('transfer function works correctly', async () => {
+        // Get the transfer receipt and check if it has the correct data.
         let transferReceipt = await this.ToniToken.transfer(accounts[1], 250000, { from: accounts[0] });
         assert.equal(transferReceipt.logs.length, 1, 'triggers an event');
         assert.equal(transferReceipt.logs[0].event, 'Transfer', 'triggers a "transfer" event');
         assert.equal(transferReceipt.logs[0].args._from, accounts[0], 'logs the account the tokens are transferred from');
         assert.equal(transferReceipt.logs[0].args._to, accounts[1], 'logs the account the tokens are transferred to');
         assert.equal(transferReceipt.logs[0].args._value, 250000, 'logs the transfer amount');
-
+        
+        // Check if value goes from sender to receiver's account.
         let receiverBalance = await this.ToniToken.balanceOf(accounts[1]);
         let senderBalance = await this.ToniToken.balanceOf(accounts[0]);
         assert.equal(receiverBalance.toNumber(), 250000, 'adds the ammount to the receiver account');
         assert.equal(senderBalance.toNumber(), 750000, 'deducts the ammount from sender account');
-        // Check if it returns a boolean.
+        
+        // Get the function's output and check if it's a boolean.
         let output = await this.ToniToken.transfer.call(accounts[1], 250000);
+        assert.equal(output, true, 'returns true');
+    })
+
+    // Test the "approve" function.
+    it('approves tokens for delegated transfer', async () => {
+        // Get the transaction receipt and check if it has the correct data.
+        let transactionReceipt = await this.ToniToken.approve(accounts[1], 100, { from: accounts[0] });
+        assert.equal(transactionReceipt.logs.length, 1, 'triggers an event');
+        assert.equal(transactionReceipt.logs[0].event, 'Approval', 'triggers an "Approval" event');
+        assert.equal(transactionReceipt.logs[0].args._owner, accounts[0], 'logs the account the tokens are transferred from');
+        assert.equal(transactionReceipt.logs[0].args._spender, accounts[1], 'logs the account the tokens are transferred to');
+        assert.equal(transactionReceipt.logs[0].args._value, 100, 'logs the transfer amount');
+        
+        // Check if it sets the correct allowance.
+        let allowance = await this.ToniToken.allowance(accounts[0], accounts[1]);
+        assert.equal(allowance.toNumber(), 100, 'sets the correct allowance for the delegated transfer');
+        
+        // Get the function's output and check if it's a boolean.
+        let output = await this.ToniToken.approve.call(accounts[1], 100);
+        assert.equal(output, true, 'returns true');
+    })
+
+    // The the "transferFrom" function.
+    it('correctly transacts the delegated transfers', async () => {
+        let fromAccount = accounts[2];  // account spending the tokens
+        let toAccount = accounts[3];    // account receiving the tokens
+        let spendingAccount = accounts[4];  // account making the transfer
+        // Transfer 100 tokens to fromAccount to test the method.
+        let initialTransferReceipt = await this.ToniToken.transfer(fromAccount, 100, { from: accounts[0] });
+        // Approve spendingAccount to spend 10 tokens belonging to fromAccount.
+        let approveTransferReceipt = await this.ToniToken.approve(spendingAccount, 10, { from: fromAccount });
+        
+        
+        // Get the function's output and check if it's a boolean.
+        let output = await this.ToniToken.transferFrom.call(accounts[1], 100);
         assert.equal(output, true, 'returns true');
     })
 })
