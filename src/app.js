e0a9fc3714e60a58546921be0df9c940ef3a962d
aantoniorodrigues <84176351+aantoniorodrigues@users.noreply.github.com>
2021-10-22 17:52:56 +0100

    Add files via upload
    Javascript file to control the app's flow

---
+++ src/app.js
@@ -0,0 +1,15 @@
+App = {
+    // Initializes the app.
+    init: function() {
+        console.log('App initialized...');
+    }
+
+    // Instantiates web3.
+}
+
+// Initialize the app.
+$(function() {
+    $(window).load(() => {
+        App.init();
+    })
+});
